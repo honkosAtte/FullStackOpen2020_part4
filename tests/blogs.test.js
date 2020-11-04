6e0671ce2ff3e0f63c953fd6f7ba12bf719970c4
Atte Honkonen <ext.atte.honkonen@aditro.com>
2020-11-04 22:14:20 +0200

    4.5-4.7

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -44,3 +44,175 @@ describe('total likes', () => {
   })
 
 })
+
+describe('favoriteBlog', () => {
+  const listWithThreeBlogs = [
+    {
+      _id: '',
+      title: 'One',
+      author: '',
+      url: '',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '',
+      title: 'Max',
+      author: '',
+      url: '',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '',
+      title: 'Max2',
+      author: '',
+      url: '',
+      likes: 10,
+      __v: 0
+    }
+  ]
+
+  
+  test('when list has only one blog return that one blog', () => {
+    const result = listHelper.favoriteBlog([listWithThreeBlogs[0]])
+    expect(result).toEqual( { _id: '', title: 'One', author: '', url: '', likes: 5, __v: 0 } )
+  })
+
+  test('when list has two or more equals returns the first one which has max likes', () => {
+    const result = listHelper.favoriteBlog(listWithThreeBlogs)
+    expect(result).toEqual( { _id: '', title: 'Max', author: '', url: '', likes: 10, __v: 0 } )
+  })
+
+  test('when list is empty equals undefined', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toBe(undefined)
+  })
+
+})
+
+describe('mostBlogs', () => {
+  const listWithThreeBlogs = [
+    {
+      _id: '',
+      title: '',
+      author: 'Jorma',
+      url: '',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '',
+      title: '',
+      author: 'Jorma',
+      url: '',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '',
+      title: '',
+      author: 'Kake',
+      url: '',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '',
+      title: '',
+      author: 'Kake',
+      url: '',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '',
+      title: '',
+      author: 'Sepi',
+      url: '',
+      likes: 10,
+      __v: 0
+    }
+  ]
+
+  
+  test('when list has only one blog return that one blog author and blog count of 1', () => {
+    const result = listHelper.mostBlogs([listWithThreeBlogs[0]])
+    expect(result).toEqual( { author: 'Jorma', blogs: 1 } )
+  })
+
+  test('when list has two or more equals returns an author with most blogs and the count of blogs', () => {
+    const result = listHelper.mostBlogs(listWithThreeBlogs)
+    expect(result).toEqual( { author: 'Jorma', blogs: 2 } )
+  })
+
+  test('when list is empty equals undefined', () => {
+    const result = listHelper.mostBlogs([])
+    expect(result).toBe(undefined)
+  })
+
+})
+
+
+describe('mostLikesInBlogs', () => {
+  const listWithThreeBlogs = [
+    {
+      _id: '',
+      title: '',
+      author: 'Jorma',
+      url: '',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '',
+      title: '',
+      author: 'Jorma',
+      url: '',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '',
+      title: '',
+      author: 'Kake',
+      url: '',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '',
+      title: '',
+      author: 'Kake',
+      url: '',
+      likes: 3,
+      __v: 0
+    },
+    {
+      _id: '',
+      title: '',
+      author: 'Sepi',
+      url: '',
+      likes: 10,
+      __v: 0
+    }
+  ]
+
+  
+  test('when list has only one blog return that one blog author and blog count of 1', () => {
+    const result = listHelper.mostLikes([listWithThreeBlogs[0]])
+    console.log(result)
+    expect(result).toEqual( { author: 'Jorma', likes: 5 } )
+  })
+
+  test('when list has two or more equals returns an author with most likes in blogs', () => {
+    const result = listHelper.mostLikes(listWithThreeBlogs)
+    expect(result).toEqual( { author: 'Jorma', likes: 15 } )
+  })
+
+  test('when list is empty equals undefined', () => {
+    const result = listHelper.mostLikes([])
+    expect(result).toBe(undefined)
+  })
+
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,10 +1,9 @@
+const { xorBy } = require('lodash');
+const _ = require('lodash');
+
 const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => {
-if (blogs === []) {
-  return 0
-}
-
   const reducer = (acc, curr) => {
     return acc + curr.likes
   }
@@ -12,4 +11,37 @@ if (blogs === []) {
 
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+  const maxLike = Math.max(...blogs.map(blog => blog.likes))
+  return blogs.filter(blog => blog.likes === maxLike)[0]
+}
+
+
+
+const mostBlogs = (blogs) => {
+  if (blogs.length < 1) {
+    return undefined
+  }
+  else {
+    const mostBlogs = _(blogs).countBy(x => x.author).entries().maxBy(1)
+    return { author: mostBlogs[0], blogs: mostBlogs[1] }
+  }
+}
+
+
+const mostLikes = (blogs) => {
+  const groupedByAuthor = _.groupBy(blogs, 'author')
+  const flattedList = _.map(groupedByAuthor, author => author)
+  const temp = []
+
+  flattedList.forEach(row => {
+    const sum = _.sumBy(row, r => r.likes)
+    const nameForTesti1 = _.map(row, r => r.author)
+    temp.push({ author: nameForTesti1[0], likes: sum })
+  })
+
+  return _.maxBy(temp, l => l.likes)
+}
+
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes }
